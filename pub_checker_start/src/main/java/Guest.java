750fdef3f6a28dde825a98f46df3c13798867f6d
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2023-01-26 15:55:31 +0000

    Properties, Constructor , Getters & Setters applied to Guest.java

--- pub_checker_start/src/main/java/Guest.java
+++ pub_checker_start/src/main/java/Guest.java
@@ -1,8 +1,59 @@
 public class Guest {
 
     private String name;
+    private char currency;
+    private int wallet;
+    private int sobriety;
+    private boolean isBanned;
 
-    public Guest(String name){
+
+    public Guest(String name,int wallet,int sobriety,boolean isBanned, char currency){
         this.name = name;
+        this.currency = currency;
+        this.wallet = wallet;
+        this.sobriety = sobriety;
+        this.isBanned = isBanned;
+    }
+
+    // GETTERS & SETTERS
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(char currency){
+        this.currency = currency;
+    }
+
+    public int getWallet (){
+        return wallet;
+    }
+
+    public void setWallet(int wallet) {
+        this.wallet = wallet;
+    }
+
+    public int getSobriety(){
+        return sobriety;
+    }
+
+    public void setSobriety(int sobriety){
+        this.sobriety = sobriety;
+    }
+
+    public boolean getIsBanned(){
+        return isBanned;
+    }
+
+    public void setBanned(boolean banned) {
+        isBanned = banned;
     }
 }
